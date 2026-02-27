20655f192168b30a87520c01dbf8056e68f3278f
bobas17 <bobascharles@gmail.com>
2026-02-27 22:35:13 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>IT Infrastructure Management System</h1>
+
+      <h3>Modules</h3>
+      <ul>
+        <li>Service Catalog</li>
+        <li>Availability Management</li>
+        <li>Information Security</li>
+        <li>Capacity Management</li>
+        <li>Continuity Management</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
